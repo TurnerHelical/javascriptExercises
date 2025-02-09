9980c992466b697054e0a64df026186a86d47e88
Hunter <hunterde91@gmail.com>
2025-02-09 11:35:39 -0500

    solution to 04, function passed all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -3,25 +3,32 @@
 
 // Set up arguments including the ability to add multiple arguments
 // find the index of the number to be removed and then use splice to cut that item out
-function removeFromArray(array, ...remove) {
-    
+function removeFromArray(array, ...removes) {
+   for (remove of removes) {
+    while (array.includes(remove)) {
+        let index = array.indexOf(remove);
+        array.splice(index,1);
+    } 
+}
+    return array
 };
-function getArray() {
-    let numArray = prompt('Please enter the numbers you want in the array seperated by a space.').split(' ');
-    console.log(numArray); 
-    return numArray;
-}
+// function getArray() {
+//     let numArray = prompt('Please enter the numbers you want in the array seperated by a space.').split(' ');
+//     console.log(numArray); 
+//     return numArray;
+// }
 
-function numToRemove() {
-    let num = prompt('What numbers do you want to remove. List them seperated by a space if multiple.')
-    let numberToRemove = num.split(' ');
-    console.log(numberToRemove);
-    return numberToRemove;
+// function numToRemove() {
+//     let num = prompt('What numbers do you want to remove. List them seperated by a space if multiple.')
+//     let numberToRemove = num.split(' ');
+//     console.log(numberToRemove);
+//     return numberToRemove;
 
-}
-let array = getArray();
-let remove = numToRemove();
-let newArray = removeFromArray(array,remove);
+// }
+// let array = getArray();
+// let remove = numToRemove();
+// let newArray = removeFromArray(array,remove);
 // let newArray = removeFromArray()
+
 // Do not edit below this line
-// module.exports = removeFromArray;
+module.exports = removeFromArray;
