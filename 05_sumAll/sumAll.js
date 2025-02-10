73561771c6019e0168ebd29abd3f1f19e65e294d
TurnerHelical <hunterde91@gmail.com>
2025-02-10 11:34:13 -0500

    added solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+   let sum = 0;
+    for (let i = firstNumber; i <= secondNumber; i++) {
+        sum += i;
+    }
+    return sum
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+// Function that takes 2 given numbers and adds all the numbers between
+// them into aa final sum
+    // Create variable to hold the final sum
+    // Needs to loop through all the numbers between the given numbers
+    // and add them up and store in the final sum
+    // The final sum needs to be returned
+
+
+// EX: 2,6 needs to loop starting from 2 and add 3 then store that value
+// in the final sum
+// then 3 + 4 and store that value in the final Sum
+// then 4 + 5 and store in final sum
+// then 5 + 6 and store in final sum
+// then the final sum needs to be returned
